5691ced383e0324d3204b82c9340d7d75599907e
ShuriIkeno <nogi.akari0305@gmail.com>
2025-03-21 01:45:51 +0900

    Remove ignored files

--- dataconnect-generated/js/default-connector/index.cjs.js
+++
@@ -1,8 +0,0 @@
-
-const connectorConfig = {
-  connector: 'default',
-  service: 'y',
-  location: 'asia-east1'
-};
-exports.connectorConfig = connectorConfig;
-
--- src/types/Paper.ts
+++
@@ -1,8 +0,0 @@
-export interface Paper {
-    id?: string;         // Firestoreで自動付与されるID
-    userId: string;      // このドキュメントを作成したユーザーのUID
-    authors: string[];   // 著者名の配列
-    pdfUrl: string;      // PDFファイルのダウンロードURL
-    createdAt: number;   // 登録日時 (UNIXタイムなど)
-  }
-  
